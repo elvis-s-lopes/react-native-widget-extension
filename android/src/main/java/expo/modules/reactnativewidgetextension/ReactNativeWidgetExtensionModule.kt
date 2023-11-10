a0b3525417765576fad7b11d98574f00cb5ef7cb
Elvis Lopes <58557455+elvis-s-lopes@users.noreply.github.com>
2023-11-10 15:05:31 -0300

    add android support

---
+++ android/src/main/java/expo/modules/reactnativewidgetextension/ReactNativeWidgetExtensionModule.kt
@@ -0,0 +1,39 @@
+package expo.modules.reactnativewidgetextension
+import android.util.Log
+import android.content.Context
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
+import android.app.Activity;
+import android.app.Application;
+import android.content.pm.PackageManager;
+import android.content.Intent;
+import android.content.ComponentName;
+
+class ReactNativeWidgetExtensionModule() : Module() {
+    override fun definition() = ModuleDefinition {
+        Name("ReactNativeWidgetExtension")
+
+
+        Function("startActivity") { jsonPayload: String ->
+
+          Log.v("startActivity", "$jsonPayload")
+
+
+        }
+
+        Function("areActivitiesEnabled") {
+            true
+        }
+
+
+    }
+
+  private val context: Context
+    get() = requireNotNull(appContext.reactContext) { "React Application Context is null" }
+  
+  private val currentActivity
+    get() = requireNotNull(appContext.activityProvider?.currentActivity)
+
+  private val pm
+    get() = requireNotNull(currentActivity.packageManager)
+}
