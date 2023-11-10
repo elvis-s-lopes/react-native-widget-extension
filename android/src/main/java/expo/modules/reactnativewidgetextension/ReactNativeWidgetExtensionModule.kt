9d451605ff5957e323caea870aa0cf4f6d523773
Elvis Lopes <58557455+elvis-s-lopes@users.noreply.github.com>
2023-11-10 16:45:50 -0300

    add new files

--- android/src/main/java/expo/modules/reactnativewidgetextension/ReactNativeWidgetExtensionModule.kt
+++ android/src/main/java/expo/modules/reactnativewidgetextension/ReactNativeWidgetExtensionModule.kt
@@ -9,16 +9,54 @@ import android.content.pm.PackageManager;
 import android.content.Intent;
 import android.content.ComponentName;
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
 // add news class 
 
 class ReactNativeWidgetExtensionModule() : Module() {
+
+    private fun createNotificationChannel(context: Context) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Channel name"
+            val descriptionText = "Channel description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("channel_id", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
     override fun definition() = ModuleDefinition {
         Name("ReactNativeWidgetExtension")
 
 
         Function("startActivity") { jsonPayload: String ->
 
-          Log.v("startActivity", "$jsonPayload")
+            Log.v("startActivity", "$jsonPayload")
+
+            // Create a notification and show it
+            val builder = NotificationCompat.Builder(context, "channel_id")
+                .setSmallIcon(R.drawable.redbox_top_border_background) // Replace with your notification icon
+                .setContentTitle("Notification Title")
+                .setContentText("Notification Content")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            createNotificationChannel(context)
+            with(NotificationManagerCompat.from(context)) {
+                notify(1, builder.build()) // 1 is a unique ID for the notification
+            }
+
+
 
 
         }
