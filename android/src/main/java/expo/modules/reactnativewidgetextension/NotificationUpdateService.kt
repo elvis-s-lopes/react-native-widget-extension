5f2758a4f2065721e573ea881c17faafb3557dbf
Elvis Lopes <58557455+elvis-s-lopes@users.noreply.github.com>
2023-11-13 17:55:09 -0300

    add news

---
+++ android/src/main/java/expo/modules/reactnativewidgetextension/NotificationUpdateService.kt
@@ -0,0 +1,225 @@
+package expo.modules.reactnativewidgetextension
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.Service
+import android.content.Intent
+import android.graphics.*
+import android.os.IBinder
+import android.os.Handler
+import android.os.Looper
+import androidx.core.app.NotificationCompat
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.widget.RemoteViews
+import android.util.DisplayMetrics
+import kotlin.random.Random
+import android.os.PowerManager
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.util.Log
+
+
+class NotificationUpdateService : Service() {
+    private val handler = Handler(Looper.getMainLooper())
+    private lateinit var runnable: Runnable
+    private val CHANNEL_ID = "order-status"
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel(this)
+        startForeground(1, createInitialNotification())
+    }
+
+    private fun createNotificationChannel(context: Context) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "live-activities"
+            val descriptionText = "Channel description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("order-status", name, importance).apply {
+                description = descriptionText
+            }
+
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+
+        runnable = Runnable {
+            updateNotification()
+            handler.postDelayed(runnable, 20000) // Update every 20 seconds
+        }
+        handler.post(runnable)
+        return START_STICKY
+    }
+
+    private fun vibrateDevice() {
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            @Suppress("DEPRECATION")
+            vibrator.vibrate(1000)
+        }
+    }
+
+    private fun wakeUpDevice() {
+        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        val wakeLock = powerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP, "MyApp:WakeLockTag")
+        wakeLock.acquire(3000) // Acende a tela por 3 segundos
+        wakeLock.release() // É importante liberar o WakeLock após o uso
+    }
+
+    private fun createInitialNotification(): Notification {
+        val customView = createCustomNotificationView("Inicializando...")
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Serviço em Segundo Plano")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+            .setCustomContentView(customView)
+        return builder.build()
+    }
+
+    private fun updateNotification() {
+        val currentTime = System.currentTimeMillis().toString()
+        val customView = createCustomNotificationView("Atualizado às: $currentTime")
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setCustomContentView(customView)
+            .build()
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(1, notification)
+
+        //vibrateDevice()
+        wakeUpDevice()
+    }
+
+    private fun createCustomNotificationView(text: String): RemoteViews {
+        val displayMetrics = resources.displayMetrics
+        val screenWidth = displayMetrics.widthPixels
+        val density = displayMetrics.density
+
+        val marginDp = 16f
+        val marginPx = (marginDp * density).toInt()
+        var bitmapWidth = screenWidth - (marginPx * 2)
+
+        if (android.os.Build.VERSION.SDK_INT == android.os.Build.VERSION_CODES.P) {
+            bitmapWidth = screenWidth * 2
+        }
+        val randomProgress = Random.nextInt(1, 70)
+
+        val stopPoints = floatArrayOf(0.25f, 0.5f, 0.75f)
+        val progressWithCarBitmap = createProgressBitmapWithDashedLine(this, bitmapWidth, randomProgress, stopPoints)
+
+        val customView = RemoteViews(packageName, R.layout.notification_layout).apply {
+            setTextViewText(R.id.tvTitle, text)
+            setTextViewText(R.id.tvCarDetails, "Elvis Lopes")
+            setImageViewBitmap(R.id.imageProgress, progressWithCarBitmap)
+        }
+
+        return customView
+    }
+
+    private fun scaleBitmap(bitmap: Bitmap, newWidth: Int, newHeight: Int): Bitmap {
+        return Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true)
+    }
+
+    private fun createProgressBitmapWithDashedLine(
+        context: Context,
+        width: Int,
+        progress: Int,
+        stopPoints: FloatArray
+    ): Bitmap {
+        val resources = context.resources
+        val carBitmap = BitmapFactory.decodeResource(resources, R.drawable.car_mini)
+
+        val newCarWidth = 130
+        val newCarHeight = 80
+        val scaledCarBitmap = scaleBitmap(carBitmap, newCarWidth, newCarHeight)
+
+        val carWidth = scaledCarBitmap.width
+        val carHeight = scaledCarBitmap.height
+        val barHeight = 25f
+
+        val totalWidth = width + carWidth
+        val bitmap = Bitmap.createBitmap(totalWidth, carHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            style = Paint.Style.FILL
+        }
+
+        // Barra de progresso de fundo
+        val progressRect = RectF(
+            (carWidth / 2).toFloat(),
+            (carHeight - barHeight) / 2,
+            width.toFloat() + (carWidth / 2),
+            (carHeight + barHeight) / 2
+        )
+        paint.color = Color.parseColor("#d5d3db")
+        canvas.drawRoundRect(progressRect, barHeight / 2, barHeight / 2, paint)
+
+        // Progresso preenchido
+        val progressLength = progressRect.width() * (progress / 100f)
+        val progressRectFilled = RectF(
+            progressRect.left,
+            progressRect.top,
+            progressRect.left + progressLength,
+            progressRect.bottom
+        )
+        paint.color = Color.parseColor("#2761f0")
+        canvas.drawRoundRect(progressRectFilled, barHeight / 2, barHeight / 2, paint)
+
+        // Linha pontilhada para o progresso não completo
+        val dashedLinePaint = Paint(paint).apply {
+            color = Color.WHITE
+            style = Paint.Style.STROKE
+            strokeWidth = barHeight
+            pathEffect = DashPathEffect(floatArrayOf(60f, 70f), 0f)
+        }
+
+        // Desenhar a linha pontilhada na seção não completa da barra de progresso
+        if (progress < 100) {
+            val path = Path()
+            path.moveTo(progressRectFilled.right, progressRect.centerY())
+            path.lineTo(progressRect.right, progressRect.centerY())
+            canvas.drawPath(path, dashedLinePaint)
+        }
+
+        // Desenhar os pontos de parada
+        stopPoints.sorted().forEach { stopPoint ->
+            val stopX = progressRect.left + (stopPoint * progressRect.width())
+            paint.color = Color.parseColor("#a7bce3") // Cor da borda do ponto de parada
+            canvas.drawCircle(stopX, progressRect.centerY(), 20f, paint)
+            paint.color = Color.parseColor("#2761f0") // Cor do ponto de parada
+            canvas.drawCircle(stopX, progressRect.centerY(), 10f, paint)
+        }
+
+        // Desenhar o carro redimensionado
+        val carX = progressRectFilled.right - carWidth / 2f
+        canvas.drawBitmap(scaledCarBitmap, carX, (carHeight - scaledCarBitmap.height) / 2f, null)
+
+        // Reciclar os bitmaps se não forem mais necessários
+        carBitmap.recycle()
+        scaledCarBitmap.recycle()
+
+        return bitmap
+    }
+
+    override fun onDestroy() {
+        handler.removeCallbacks(runnable)
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+}
--- android/src/main/java/expo/modules/reactnativewidgetextension/ReactNativeWidgetExtensionModule.kt
+++ android/src/main/java/expo/modules/reactnativewidgetextension/ReactNativeWidgetExtensionModule.kt
@@ -4,52 +4,27 @@ import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 import android.util.Log
 import com.google.gson.Gson
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import java.net.URL
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.DashPathEffect
-import android.graphics.Paint
-import android.graphics.Path
-import android.graphics.RectF
-import android.os.Build
-import android.widget.RemoteViews
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
+import android.content.Intent
+import androidx.core.content.ContextCompat.startForegroundService
+import java.io.Serializable
 
 // add news class 
 
 class ReactNativeWidgetExtensionModule() : Module() {
 
-    private fun createNotificationChannel(context: Context) {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "Channel name"
-            val descriptionText = "Channel description"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel("channel_id", name, importance).apply {
-                description = descriptionText
-            }
-            // Register the channel with the system
-            val notificationManager: NotificationManager =
-                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
+
 
     override fun definition() = ModuleDefinition {
         Name("ReactNativeWidgetExtension")
 
         Function("startActivity") { jsonPayload: String ->
 
+
             try {
                 Log.i("startActivity()", "startActivity()")
+
+                Result.success("Serviço de notificação iniciado")
 
                 val payload = Gson().fromJson(jsonPayload, ActivityPayload::class.java)
 
@@ -57,63 +32,17 @@ class ReactNativeWidgetExtensionModule() : Module() {
                 val avatarMiniFileName = URL(payload.avatarMini).path
                 val carImageFileName = URL(payload.carImage).path
 
-                Log.i("Avatar Mini File Name", "Error: ${avatarMiniFileName}")
-                Log.i("Car Image File Name", "Error: ${carImageFileName}")
+                //Log.i("Avatar Mini File Name", "Error: ${avatarMiniFileName}")
+                //Log.i("Car Image File Name", "Error: ${carImageFileName}")
+
+                // Iniciar o serviço de notificação
+                val serviceIntent = Intent(context, NotificationUpdateService::class.java)
+                //serviceIntent.putExtra("payloadKey", payload as Serializable)
+                startForegroundService(context, serviceIntent)
 
 
 
 
-                val displayMetrics = context.resources.displayMetrics
-                val screenWidth = displayMetrics.widthPixels
-                val density = displayMetrics.density
-
-                // Defina uma margem padrão em dp que será convertida em pixels. Ajuste este valor conforme necessário.
-                val marginDp = 16f // ou outro valor que melhor se adapte ao layout desejado
-                // Converta a margem dp para pixels baseada na densidade da tela
-                val marginPx = (marginDp * density).toInt()
-
-                // Ajuste a largura do bitmap de acordo com as margens. Subtraia as margens do lado esquerdo e direito.
-                var bitmapWidth = screenWidth - (marginPx * 2)
-
-                // Verifique a versão do Android, e se for Android 9 (API level 28), ajuste a largura conforme necessário
-                if (Build.VERSION.SDK_INT == Build.VERSION_CODES.P) {
-                    bitmapWidth = screenWidth * 2 // Ajuste a largura do bitmap subtraindo a margem adicional de ambos os lados
-                }
-
-                val stopPoints = floatArrayOf(0.25f, 0.5f, 0.75f) // Pontos de parada em 25%, 50%, e 75%
-                val progressWithCarBitmap = createProgressBitmapWithDashedLine(context, bitmapWidth, 80, stopPoints)
-
-                val customView = RemoteViews(context.packageName, R.layout.notification_layout).apply {
-                    setTextViewText(R.id.tvTitle, "Estacionado há 0 min")
-                    setTextViewText(R.id.tvCarDetails, "Elvis Lopes")
-                    setImageViewBitmap(R.id.imageProgress, progressWithCarBitmap)
-                }
-
-
-                val customViewBig = RemoteViews(context.packageName, R.layout.notification_layout).apply {
-                    setTextViewText(R.id.tvTitle, "Estacionado há 0 min")
-                    setTextViewText(R.id.tvCarDetails, "FHN1230: Vermelho Toyota Etios")
-
-                }
-
-                // Create a notification and show it
-                val builder = NotificationCompat.Builder(context, "channel_id")
-                    .setContentTitle("")
-                    .setShowWhen(false)
-                    .setSmallIcon(R.drawable.ic_launcher_foreground)
-                    .setPriority(NotificationCompat.PRIORITY_HIGH)
-                    .setStyle(NotificationCompat.DecoratedCustomViewStyle())
-                    .setCustomContentView(customView)
-                    .setCustomBigContentView(customViewBig)
-                    .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                    .setAutoCancel(false)
-
-
-
-                createNotificationChannel(context)
-                with(NotificationManagerCompat.from(context)) {
-                    notify(1, builder.build()) // 1 is a unique ID for the notification
-                }
 
                 Result.success("Some result or token")
             } catch (e: Exception) {
@@ -128,94 +57,6 @@ class ReactNativeWidgetExtensionModule() : Module() {
         }
 
 
-    }
-
-    fun scaleBitmap(bitmap: Bitmap, newWidth: Int, newHeight: Int): Bitmap {
-        return Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true)
-    }
-
-    fun createProgressBitmapWithDashedLine(
-        context: Context,
-        width: Int,
-        progress: Int,
-        stopPoints: FloatArray,
-    ): Bitmap {
-        val resources = context.resources
-        val carBitmap = BitmapFactory.decodeResource(resources, R.drawable.car_mini)
-
-        // Novas dimensões para o carro
-        val newCarWidth = 130 // Substitua com a largura desejada
-        val newCarHeight = 80 // Substitua com a altura desejada
-        val scaledCarBitmap = scaleBitmap(carBitmap, newCarWidth, newCarHeight)
-
-        val carWidth = scaledCarBitmap.width
-        val carHeight = scaledCarBitmap.height
-        val barHeight = 25f // Altura da barra de progresso
-
-        val totalWidth = width + carWidth
-        val bitmap = Bitmap.createBitmap(totalWidth, carHeight, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
-
-        val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-            style = Paint.Style.FILL
-        }
-
-        // Barra de progresso de fundo
-        val progressRect = RectF(
-            (carWidth / 2).toFloat(),
-            (carHeight - barHeight) / 2,
-            width.toFloat() + (carWidth / 2),
-            (carHeight + barHeight) / 2
-        )
-        paint.color = Color.parseColor("#d5d3db")
-        canvas.drawRoundRect(progressRect, barHeight / 2, barHeight / 2, paint)
-
-        // Progresso preenchido
-        val progressLength = progressRect.width() * (progress / 100f)
-        val progressRectFilled = RectF(
-            progressRect.left,
-            progressRect.top,
-            progressRect.left + progressLength,
-            progressRect.bottom
-        )
-        paint.color = Color.parseColor("#2761f0")
-        canvas.drawRoundRect(progressRectFilled, barHeight / 2, barHeight / 2, paint)
-
-        // Linha pontilhada para o progresso não completo
-        val dashedLinePaint = Paint(paint).apply {
-            color = Color.WHITE
-            style = Paint.Style.STROKE
-            strokeWidth = barHeight
-            pathEffect = DashPathEffect(floatArrayOf(60f, 70f), 0f)
-        }
-
-        // Desenhar a linha pontilhada na seção não completa da barra de progresso
-        if (progress < 100) {
-            val path = Path()
-            path.moveTo(progressRectFilled.right, progressRect.centerY())
-            path.lineTo(progressRect.right, progressRect.centerY())
-            canvas.drawPath(path, dashedLinePaint)
-        }
-
-        // Desenhar os pontos de parada
-        paint.style = Paint.Style.FILL
-        stopPoints.sorted().forEach { stopPoint ->
-            val stopX = progressRect.left + (stopPoint * progressRect.width())
-            paint.color = Color.parseColor("#a7bce3") // Cor da borda do ponto de parada
-            canvas.drawCircle(stopX, progressRect.centerY(), 20f, paint)
-            paint.color = Color.parseColor("#2761f0") // Cor do ponto de parada
-            canvas.drawCircle(stopX, progressRect.centerY(), 10f, paint)
-        }
-
-        // Desenhar o carro redimensionado
-        val carX = progressRectFilled.right - carWidth / 2f
-        canvas.drawBitmap(scaledCarBitmap, carX, (carHeight - scaledCarBitmap.height) / 2f, null)
-
-        // Reciclar os bitmaps se não forem mais necessários
-        carBitmap.recycle()
-        scaledCarBitmap.recycle()
-
-        return bitmap
     }
 
 data class ActivityPayload(
