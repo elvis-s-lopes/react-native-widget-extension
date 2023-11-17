768b983becb6745bf47461d3880687b18a483b74
Elvis Lopes <58557455+elvis-s-lopes@users.noreply.github.com>
2023-11-17 17:44:16 -0300

    add

--- android/src/main/java/expo/modules/reactnativewidgetextension/NotificationUpdateService.kt
+++ android/src/main/java/expo/modules/reactnativewidgetextension/NotificationUpdateService.kt
@@ -204,7 +204,7 @@ class NotificationUpdateService : Service() {
         val stopServicePendingIntent = createStopServicePendingIntent()
 
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setSmallIcon(R.drawable.ic_baseline_notifications_24)
             .setStyle(NotificationCompat.DecoratedCustomViewStyle())
             .setCustomBigContentView(customViewExp)
             .setCustomContentView(customView)
